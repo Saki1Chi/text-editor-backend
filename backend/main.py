613ac21e4e74d5381ff21a7e869cbae4e965b138
Saki1Chi <shmhh216000@gmail.com>
2025-08-16 20:54:57 -0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,11 @@ app = FastAPI(
 # CORS para Vite dev server
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173", 
+        "http://127.0.0.1:5173",
+        "https://text-editor-frontend-mdt7.onrender.com"  # <- tu URL pública de Render
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
