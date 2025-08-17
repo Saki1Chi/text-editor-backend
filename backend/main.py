fb5621e04bf5333040d4894715c3f37499d7e993
Saki1Chi <shmhh216000@gmail.com>
2025-08-16 20:06:39 -0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,13 @@ from pydantic import BaseModel, EmailStr
 import sqlite3, os, time, bcrypt, jwt
 from datetime import datetime, timedelta
 import logging
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+
 
 # Configura logging
 logging.basicConfig(level=logging.INFO)
